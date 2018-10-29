06ac3d4d8c5b46c4dfb78a8f8a67febcfcd3c5d3
liulei <llei@czbapp.com>
2018-10-29 15:53:25 +0800

    fix：add comment

--- config/config.default.js
+++ config/config.default.js
@@ -6,28 +6,28 @@
  * @property {String} SOME_KEY - some description
  */
 exports.thriftClient = {
-  // multi service
-  client1: {
-    port: '10240',
-    host: '127.0.0.1',
-    serviceClient: [
-      {
-        name: 'testService1',
-        path: '/thrift/gen-nodejs/testService1.js',
-      },
-      {
-        name: 'testService2',
-        path: '/thrift/gen-nodejs/testService2.js',
-      },
-    ],
-  },
-  // single service
-  client2: {
-    port: '10241',
-    host: '127.0.0.1',
-    serviceClient: {
-      name: 'testService',
-      path: '/thrift/gen-nodejs/testService.js',
-    },
-  },
+  // // multi service
+  // client1: {
+  //   port: '10240',
+  //   host: '127.0.0.1',
+  //   serviceClient: [
+  //     {
+  //       name: 'testService1',
+  //       path: '/thrift/gen-nodejs/testService1.js',
+  //     },
+  //     {
+  //       name: 'testService2',
+  //       path: '/thrift/gen-nodejs/testService2.js',
+  //     },
+  //   ],
+  // },
+  // // single service
+  // client2: {
+  //   port: '10241',
+  //   host: '127.0.0.1',
+  //   serviceClient: {
+  //     name: 'testService',
+  //     path: '/thrift/gen-nodejs/testService.js',
+  //   },
+  // },
 };
